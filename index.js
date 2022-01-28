404273ab3b19425ba381eac7b0bb4ecfa040aa84
natashaswan <91715868+natashaswan@users.noreply.github.com>
2022-01-28 15:17:03 -0800

    made why us section responsive, removed reviews

---
+++ index.js
@@ -0,0 +1,37 @@
+// read more button
+function readMoreClick(id) {
+    let dots = document.getElementById("dots"+id);
+    let moreText = document.getElementById("more"+id);
+    let btnText = document.getElementById("myBtn"+id);
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+//scroll back to the top button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
